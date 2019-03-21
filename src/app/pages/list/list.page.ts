41f219c95823a4b503be42eacaa9c561ee371708
Julio César Paredes <juliocepar@hotmail.com>
2019-03-21 14:00:23 -0400

    Clase 58: ion-item-sliding

--- src/app/pages/list/list.page.ts
+++ src/app/pages/list/list.page.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { DataService } from '../../services/data.service';
 import { Observable } from 'rxjs';
+import { IonList } from '@ionic/angular';
 
 @Component({
   selector: 'app-list',
@@ -9,6 +10,7 @@ import { Observable } from 'rxjs';
 })
 export class ListPage implements OnInit {
 
+  @ViewChild('lista') lista: IonList;
   usuarios: Observable<any>;
 
   constructor(private dataService: DataService) { }
@@ -16,5 +18,21 @@ export class ListPage implements OnInit {
   ngOnInit() {
     this.usuarios = this.dataService.getUsers();
   }
+
+  favorite(user) {
+    console.log('favorite', user);
+    this.lista.closeSlidingItems();
+  }
+
+  share(user) {
+    console.log('share', user);
+    this.lista.closeSlidingItems();
+  }
+
+  borrar(user) {
+    console.log('borrar', user);
+    this.lista.closeSlidingItems();
+  }
+
 
 }
